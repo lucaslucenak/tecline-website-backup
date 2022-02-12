973bcea46105b4b45910ee3491788022ffb792dc
lucaslucenak <lucas.lucenak@gmail.com>
2022-02-12 02:53:23 -0300

    added header and main routes

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+//Express import
 const express = require('express');
 const app = express();
+//EJS import
+const ejs = require('ejs');
+
+//Config
+    //View Engine
+    app.set('view engine', 'ejs');
+    app.use(express.static('public')); //Aceita arquivos estáticos
 
 
 app.get("/", (req, res) => {
-    res.send("Rota principal")
-})
+    res.render('index.ejs');
+});
+
+app.get('/requestService', (req, res) => {
+    res.render('requestService.ejs');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services.ejs');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact.ejs');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.ejs');
+});
 
 app.listen(9000, function(err) {
     if(err) {
@@ -13,4 +37,4 @@ app.listen(9000, function(err) {
     else {
         console.log("Rodando em http://localhost:9000")
     }
-})
+});
